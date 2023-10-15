ef4fb82b28b425db8229c35d8ba4b5bba1ab09f0
Naresh Jangra <nrsh13@gmail.com>
2023-10-15 15:07:32 +0530

    Latest Code Push

--- sample_job.groovy
+++ sample_job.groovy
@@ -1,41 +1,39 @@
 pipelineJob('ecs-agent-sample') {
-    displayName('ECS Agent Sample Pipeline')
-    description('A sample Jenkins job with ECS Agent')
+  displayName('ECS Agent Sample Pipeline')
+  description('A sample Jenkins job with ECS Agent')
 
-    // Declarative Pipeline section
-    definition {
-        cps {
-            script('''
-                pipeline {
-                    agent {
-					  label: 'agent'
-					}
-                    stages {
-                        stage('Checkout') {
-                            steps {
-                                checkout scm
-                            }
-                        }
-                        stage('Build and Test') {
-                            steps {
-                                script {
-                                    echo 'Hello, this is a sample Declarative Pipeline stage.'
-                                }
-                            }
-                        }
-                        stage('Deploy') {
-                            steps {
-                                script {
-                                    sh 'echo "Sample deployment steps go here"'
-                                }
-                            }
-                        }
-                    }
+  // Declarative Pipeline section
+  definition {
+    cps {
+      script('''
+        pipeline {
+          agent {
+	    label 'agent'
+	  }
+          stages {
+            stage('Checkout') {
+              steps {
+                checkout scm
+              }
+            }
+            stage('Build and Test') {
+              steps {
+                script {
+                   echo 'Hello, this is a sample Declarative Pipeline stage.'
                 }
-            ''')
-            sandbox(true)
+              }
+            }
+            stage('Deploy') {
+              steps {
+                script {
+                  sh 'echo "Sample deployment steps go here"'
+                }
+              }
+            }
+          }
         }
+     ''')
+     sandbox(true)
     }
+  }
 }
-
-
